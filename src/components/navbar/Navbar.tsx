62e5a3c3746eb23331340ff79a7a51f96faf918c
heliossandro <zenosama892@gmail.com>
2025-09-17 23:04:58 +0100

    aaaaah

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
-
+import carrinho from "../../assets/image/carrinho.png"
 
 export const NavBar = () =>{
     return(
-        <div className="">
-            <ul className="flex gap-10 mr-10 cursor-pointer ">
+        <div className="flex mr-10 cursor-pointer gap-6 ">
+            <ul className="flex gap-8 ">
                 <li className="hover:text-[#ffffff81] text-xl">Home</li>
                 <li className="hover:text-[#ffffff81] text-xl">Loja</li>
-                <li className="hover:text-[#ffffff81] text-xl">carrinho</li>
-                <li className="hover:text-[#ffffff81] text-xl">Sobre</li>
             </ul>
+            <img src={carrinho} alt="carrinho" className="w-[1.8rem] l-[1.8rem]" />
         </div>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,10 +2,31 @@ import { Header } from "../../components/header/Header"
 
 export const Home = ()=>{
     return(
-        <div>
+        <div className="">
             <Header/>
-            <div>
-                <p>Bem-Vindo, o que vai quererm doces ou salgados</p>
+            <div className="flex justify-center items-center flex-col mt-[8rem]">
+                <section className="flex items-center flex-col gap-9">
+                    <h1 className="text-[#e29db7] text-4xl mt-10 font-bold">Bem-Vindo a Confeitaria Mirella´s Delights </h1>
+                    <button className=" text-white bg-[#e29db7] text-2xl h-12 w-[150px] rounded-xl shadow-md hover:bg-[#db789e] duration-300 transform motion-safe:hover:scale-110">Encomendar</button>
+                </section>
+
+                <section className="bg-[#e29db7] flex items-center justify-center flex-col h-[30rem] w-[100%] my-[8rem]">
+                    <h1 className="text-[#ffffff] text-4xl mt-10 font-bold"> O que podes Encomendar na nossa Confeitaria</h1>
+                    <div className="">
+                        <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white">
+                        {/* <img className="w-full" alt="Imagem do card"> */}
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold mb-2">Título do Card</h2>
+                            <p className="text-gray-600 mb-4">
+                            Este é um exemplo simples de card com TailwindCSS. Podes adicionar imagem, texto e botão.
+                            </p>
+                            <button className="px-4 py-2 bg-[#e29db7] text-white rounded-lg hover:bg-[#db789e] *:transition">
+                            Saber mais
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     )
