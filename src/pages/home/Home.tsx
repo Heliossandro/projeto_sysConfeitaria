c3545c212aa935bcfb93d91b098d4f448bab7ac9
heliossandro <zenosama892@gmail.com>
2025-09-18 23:17:43 +0100

    a

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,51 @@
-import { Header } from "../../components/header/Header"
+import { useEffect, useState } from "react";
+import { Header } from "../../components/header/Header";
 
-export const Home = ()=>{
-    return(
-        <div className="">
-            <Header/>
-            <div className="flex justify-center items-center flex-col mt-[8rem]">
-                <section className="flex items-center flex-col gap-9">
-                    <h1 className="text-[#e29db7] text-4xl mt-10 font-bold">Bem-Vindo a Confeitaria Mirella´s Delights </h1>
-                    <button className=" text-white bg-[#e29db7] text-2xl h-12 w-[150px] rounded-xl shadow-md hover:bg-[#db789e] duration-300 transform motion-safe:hover:scale-110">Encomendar</button>
-                </section>
+export const Home = () => {
+  const [menus, setMenus] = useState<any[]>([]);
 
-                <section className="bg-[#e29db7] flex items-center justify-center flex-col h-[30rem] w-[100%] my-[8rem]">
-                    <h1 className="text-[#ffffff] text-4xl mt-10 font-bold"> O que podes Encomendar na nossa Confeitaria</h1>
-                    <div className="">
-                        <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white">
-                        {/* <img className="w-full" alt="Imagem do card"> */}
-                        <div className="p-4">
-                            <h2 className="text-xl font-semibold mb-2">Título do Card</h2>
-                            <p className="text-gray-600 mb-4">
-                            Este é um exemplo simples de card com TailwindCSS. Podes adicionar imagem, texto e botão.
-                            </p>
-                            <button className="px-4 py-2 bg-[#e29db7] text-white rounded-lg hover:bg-[#db789e] *:transition">
-                            Saber mais
+  useEffect(() => {
+    fetch("http://localhost:5000/menus") // chama backend
+      .then((res) => res.json())
+      .then((data) => setMenus(data))
+      .catch((err) => console.error("Erro ao buscar menus:", err));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div className="flex justify-center items-center flex-col mt-[8rem]">
+        <section className="flex items-center flex-col gap-9">
+          <h1 className="text-[#e29db7] text-4xl mt-10 font-bold">
+            Bem-Vindo a Confeitaria Mirella´s Delights
+          </h1>
+          <button className=" text-white bg-[#e29db7] text-2xl h-12 w-[150px] rounded-xl shadow-md hover:bg-[#db789e] duration-300 transform motion-safe:hover:scale-110">
+            Encomendar
+          </button>
+        </section>
+
+        <section className=" flex items-center justify-center flex-col w-[100%] my-[8rem] p-10">
+          <h1 className="text-[#e29db7] text-4xl my-10 font-bold">
+            O que podes Encomendar na nossa Confeitaria
+          </h1>
+            <div className="flex flex-col gap-8 mt-10 w-full max-w-6xl px-4">
+                {menus.map((menu) => (
+                    <div
+                        key={menu.id}
+                        className="flex flex-col sm:flex-row justify-between items-center 
+                            rounded-2xl overflow-hidden shadow-lg p-4 bg-[#e29db7] 
+                            gap-4">
+                            <h2 className="text-xl text-white font-semibold text-center sm:text-left">
+                                {menu.name}
+                            </h2>
+                            <button className="px-4 py-2 text-white rounded-lg  hover:text-[#c75c87] transition">
+                                Ver mais
                             </button>
-                        </div>
-                        </div>
                     </div>
-                </section>
+                ))}
             </div>
-        </div>
-    )
-}
+        </section>
+      </div>
+    </div>
+  );
+};
